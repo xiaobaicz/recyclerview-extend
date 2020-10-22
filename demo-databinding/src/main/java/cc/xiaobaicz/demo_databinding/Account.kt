f423ef109a33833194af423e475369f4a9da1c22
xiaobai <1063779828@qq.com>
2020-10-22 16:36:16 +0800

    add DataBinding的使用Demo

---
+++ demo-databinding/src/main/java/cc/xiaobaicz/demo_databinding/Account.kt
@@ -0,0 +1,31 @@
+package cc.xiaobaicz.demo_databinding
+
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import cc.xiaobaicz.demo_databinding.databinding.ItemAccountBinding
+
+/**
+ * 数据类
+ */
+data class Account(val icon: Int, val name: String, val phone: String) {
+
+    companion object {
+        @BindingAdapter("app:img")
+        @JvmStatic
+        fun bindImg(view: ImageView, id: Int) {
+            view.setImageResource(id)
+        }
+    }
+
+    /**
+     * 自定义支持DataBinding的ViewHolder
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val bind by lazy {
+            ItemAccountBinding.bind(view)
+        }
+    }
+
+}

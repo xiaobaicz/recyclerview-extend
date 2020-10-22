c6c2ef48bdcd952ca756911d00f6c4cb5ccc32ff
xiaobai <1063779828@qq.com>
2020-10-22 16:36:16 +0800

    新增DataBinding的使用Demo

---
+++ demo-databinding/src/main/java/cc/xiaobaicz/demo_databinding/MainActivity.kt
@@ -0,0 +1,42 @@
+package cc.xiaobaicz.demo_databinding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cc.xiaobaicz.demo_databinding.databinding.ActivityMainBinding
+import cc.xiaobaicz.recyclerview.extend.config
+
+class MainActivity: AppCompatActivity() {
+
+    private val bind by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val data = ArrayList<Any>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(bind.root)
+
+        //RecyclerView配置扩展
+        bind.list.config(data) {
+            addType<Account, Account.ViewHolder>(R.layout.item_account) { d, h, p ->
+                //数据绑定
+                with(h.bind) {
+                    setVariable(BR.account, d)
+                    executePendingBindings()
+                }
+            }
+        }
+
+        //测试数据
+        testData()
+    }
+
+    private fun testData() {
+        for (i in 0 until 100) {
+            data.add(Account(R.mipmap.ic_launcher, "Account$i", "12345678"))
+        }
+        bind.list.adapter?.notifyDataSetChanged()
+    }
+
+}

2a5a48b298a1d8858ac7dfcbbe999f81699ed5f4
laobocheng <1063779828@qq.com>
2020-10-29 21:53:08 +0800

    version 0.4

--- RecyclerViewExtend/src/main/java/cc/xiaobaicz/recyclerview/extend/ViewHolderX.kt
+++ RecyclerViewExtend/src/main/java/cc/xiaobaicz/recyclerview/extend/ViewHolderX.kt
@@ -2,9 +2,41 @@ package cc.xiaobaicz.recyclerview.extend
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import java.lang.ref.Reference
+import java.lang.ref.WeakReference
 
 /**
  * 通用ViewHolder
  * @param root Item根视图
  */
-class ViewHolderX(val root: View) : RecyclerView.ViewHolder(root)
+class ViewHolderX(val root: View) : RecyclerView.ViewHolder(root) {
+
+    /**
+     * View缓存
+     */
+    private val viewMap = HashMap<Int, Reference<View?>>()
+
+    /**
+     * 通过View ID获取视图并缓存
+     * @param resId View ID
+     * @since 0.4
+     */
+    fun <T: View> findViewById(resId: Int): T {
+        var v = viewMap[resId]  //获取View实例
+        if (v?.get() == null) { //判空
+            synchronized(ViewHolderX::class.java) {
+                //查找操作
+                v = viewMap[resId]
+                //二次判空
+                if (v?.get() == null) {
+                    //通过弱引用包装结果
+                    viewMap[resId] = WeakReference(root.findViewById(resId))
+                    v = viewMap[resId]
+                }
+            }
+        }
+        assert(v?.get() != null)    //View判空
+        return v?.get() as T
+    }
+
+}
